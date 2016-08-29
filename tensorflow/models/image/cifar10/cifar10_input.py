c73535933f2611bce92363e889c767da09f1b87d
xia <shartoo518@gmail.com>
2016-08-29 15:09:27 +0800

    add

--- tensorflow/models/image/cifar10/cifar10_input.py
+++ tensorflow/models/image/cifar10/cifar10_input.py
@@ -24,15 +24,15 @@ import os
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
-# Process images of this size. Note that this differs from the original CIFAR
-# image size of 32 x 32. If one alters this number, then the entire model
-# architecture will change and any model would need to be retrained.
+# 处理此大小的图像，注意：与原始的CIFAR 32x32图像尺寸差别。如果此值被更新，
+#那么整个模型架构将会被改变。而且模型需要重新获取
 IMAGE_SIZE = 24
 
 # Global constants describing the CIFAR-10 data set.
 NUM_CLASSES = 10
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+
 
 
 def read_cifar10(filename_queue):
